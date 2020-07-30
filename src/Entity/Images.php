82ad87791afeec7e0ac10cd54743a96d60e40b82
carlos (minimal) <padi_05@yahoo.com>
2020-07-29 21:02:14 -0400

    White space

--- src/Entity/Images.php
+++ src/Entity/Images.php
@@ -6,23 +6,23 @@ use Symfony\Contracts\Cache\ItemInterface;
 
 trait Images {
 
-	protected static $sizeCacheHours = 24 * 7;
-	protected static $sizeCacheVersion = 1;
+    protected static $sizeCacheHours = 24 * 7;
+    protected static $sizeCacheVersion = 1;
 
-	public function fetchImageInfo(string $path): array
-	{
-		$cache = new FilesystemAdapter();
-		$assetsDir = dirname(__DIR__, 2) . '/assets';
-		$urlPath = str_replace($assetsDir, '', $path);
-		$cacheKey = sprintf('image-info-v%d-%s', static::$sizeCacheVersion, str_replace(['/','.'], '-', $path));
+    public function fetchImageInfo(string $path): array
+    {
+        $cache = new FilesystemAdapter();
+        $assetsDir = dirname(__DIR__, 2) . '/assets';
+        $urlPath = str_replace($assetsDir, '', $path);
+        $cacheKey = sprintf('image-info-v%d-%s', static::$sizeCacheVersion, str_replace(['/','.'], '-', $path));
 
-		return $cache->get($cacheKey , function (ItemInterface $item) use ($path, $urlPath) {
-			$item->expiresAfter(3600 * static::$sizeCacheHours);
-			return [
-				'path' => $urlPath,
-				'getimagesize' => getimagesize($path),
-			];
-		});
-	}
+        return $cache->get($cacheKey , function (ItemInterface $item) use ($path, $urlPath) {
+            $item->expiresAfter(3600 * static::$sizeCacheHours);
+            return [
+                'path' => $urlPath,
+                'getimagesize' => getimagesize($path),
+            ];
+        });
+    }
 
 }
